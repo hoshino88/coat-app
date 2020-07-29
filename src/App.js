61dba8cabd28a5b9c0b30d8e27a6b76dbb1afc40
hoshino88 <shingo.hoshino88@gmail.com>
2020-07-29 01:37:41 +0000

    save

--- src/App.js
+++ src/App.js
@@ -3,23 +3,75 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+const members = [
+  { name: 'ゲン'},
+  { name: 'もが'},
+  { name: 'ゆり'},
+  { name: 'だい'},
+  { name: 'まい'},
+  { name: 'こば'},
+  { name: 'ツカ'},
+  { name: 'しめ'},
+  { name: 'こー'},
+  { name: 'るう'},
+  { name: 'くん'},
+  { name: 'うめ'},
+  { name: 'ゆー'},
+  { name: 'あっ'},
+  { name: 'みと'},
+];
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="boxContainer">
+            <div className="each_coat">
+                1コート
+                <ul>
+                    <li>{members[0].name}</li>
+                    <li>{members[1].name}</li>
+                    <li>{members[2].name}</li>
+                    <li>{members[3].name}</li>
+                </ul>
+                <button>終了</button>
+            </div>
+            <div className="each_coat">
+                2コート
+                <ul>
+                    <li>{members[4].name}</li>
+                    <li>{members[5].name}</li>
+                    <li>{members[6].name}</li>
+                    <li>{members[7].name}</li>
+                </ul>
+                <button>終了</button>
+            </div>
+            <div className="each_coat">
+                3コート
+                <ul>
+                    <li>{members[8].name}</li>
+                    <li>{members[9].name}</li>
+                    <li>{members[10].name}</li>
+                    <li>{members[11].name}</li>
+                </ul>
+                <button>終了</button>
+            </div>
+        </div>
+        <div className="taiki_div">
+            <ol>
+                <li>{members[12].name}</li>
+                <li>{members[13].name}</li>
+                <li>ゲン</li>
+                <li>ああ</li>
+                <li>いい</li>
+                <li>うう</li>
+                <li>ええ</li>
+                <li>おお</li>
+                <li>ああ</li>
+            </ol>
+        </div>
       </header>
-    </div>
+    </div >
   );
 }
 
