a2198f2bbd8c04f295af2b6a0d22ed1d777cc271
hoshino88 <shingo.hoshino88@gmail.com>
2020-07-29 10:34:37 +0000

    Updates

---
+++ precache-manifest.95ab6ed26c2ae2b39b30d07723fbb188.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3fbdcabf48a50b4a5dea96f3a23f872",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ea416c4405139f75e6c8",
+    "url": "./static/css/main.c89f7291.chunk.css"
+  },
+  {
+    "revision": "d52f746bc9b106af860e",
+    "url": "./static/js/2.3053cb13.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.3053cb13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea416c4405139f75e6c8",
+    "url": "./static/js/main.6b98e17e.chunk.js"
+  },
+  {
+    "revision": "3d76109aad098c88e92b",
+    "url": "./static/js/runtime-main.7d3b2c94.js"
+  }
+]);
